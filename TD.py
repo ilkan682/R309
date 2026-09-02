f3ff86771491126c9c4afb220b9ed85cc2ecf512
e2500435 <g682ilkan@gmail.com>
2026-09-02 14:47:27 +0200

    x

---
+++ TD.py
@@ -0,0 +1,11 @@
+def max (a : int, b : int)-> int :
+    if a > b:
+        print(a)
+    else :
+        print(b)
+
+
+if __name__ == '__main__':
+    val1= int(input(" saisir un entier :"))
+    val2= int(input(" saisir un entier :"))
+    print(max (val1,val2))
