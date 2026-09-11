538f23b0a65d925206deef15c1340168009149ed
e2500435 <g682ilkan@gmail.com>
2026-09-11 10:46:19 +0200

    i2

--- correction.py
+++ correction.py
@@ -2,16 +2,24 @@ import math
 
 
 class Point:
-    def __init__(self, x : int =0, y : int = 0):
+    def __init__(self, x: int = 0, y: int = 0):
         self.x = x
         self.y = y
 
-    def  __str__(self) -> str:
-        return f" Point: ({self.--x}; {self.--y})"
+    def __str__(self) -> str:
+        return f"Point: ({self.x}; {self.y})"
 
-
-    def DistanceCordonee(self,x:float, y:float)-> float :
-        Distance= math.sqrt((x-self.x)**2 + (y-self.y)**2)
-        (math.pow(self.__x-x,2)) +  math.pow(self.__y-y,2))
-        ((self.__x-x)*(self.__x-x)+(self.__y-y)*(self.__y-y))
+    def DistanceCordonee(self, x: float, y: float) -> float:
+        Distance = math.sqrt((x - self.x) ** 2 + (y - self.y) ** 2)
+        (math.pow(self.x - x, 2)) + math.pow(self.y - y, 2)
+        ((self.x - x) * (self.x - x) + (self.y - y) * (self.y - y))
         return Distance
+
+
+if __name__ == "__main__":
+    p1 = Point(3.2, 1)
+    print(p1)
+    p2 = Point()
+    print(p2)
+
+
